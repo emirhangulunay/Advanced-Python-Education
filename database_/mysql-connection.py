8a9ded4d07098cd61ba6ed6e87e01018b6ecc447
Emirhan Gülünay <emirr_eemir@hotmail.com>
2026-09-01 17:26:44 +0300

    database bağlantısı yapıldı

---
+++ database_/mysql-connection.py
@@ -0,0 +1,21 @@
+import mysql.connector
+
+db = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    password = "emirhangulunay"
+    database = "example"
+)
+
+cursor = db.cursor()
+
+# cursor.execute("Create Database example database")
+# cursor.execute("Shown Databases")
+
+
+cursor.execute("CREATE TABLE categories (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255) )")
+
+cursor.execute("SHOW TABLES")
+
+for i in cursor:
+    print(i)
